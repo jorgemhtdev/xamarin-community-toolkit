360843642a2e87216722d75ca13050450ab829f8
jorgemhtdev <jorge@jorgemht.dev>
2021-02-04 10:19:40 +0100

    [Added] - First Part

---
+++ ToolkitXamarin/ToolkitXamarin/ToolkitXamarin/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ToolkitXamarin
+{
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
