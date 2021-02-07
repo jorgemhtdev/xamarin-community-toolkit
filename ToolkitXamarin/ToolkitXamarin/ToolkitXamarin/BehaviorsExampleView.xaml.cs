a8fea2f4b18d0b3d834f2a9cb6890b9213dbf5ab
jorgemhtdev <jorge@jorgemht.dev>
2021-02-07 23:17:59 +0100

    [Added] - Converter Example View

---
+++ ToolkitXamarin/ToolkitXamarin/ToolkitXamarin/BehaviorsExampleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ToolkitXamarin
+{
+    public partial class BehaviorsExampleView : ContentPage
+    {
+        public BehaviorsExampleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ToolkitXamarin/ToolkitXamarin/ToolkitXamarin/ConverterExampleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ToolkitXamarin
+{
+    public partial class ConverterExampleView : ContentPage
+    {
+        public ConverterExampleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
